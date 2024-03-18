2688aa607ecb5cc9e348f64c91ed48d4581642ae
Subash Gupta <49033079+guptazi@users.noreply.github.com>
2024-03-18 16:47:21 -0500

    Update sgd.py

--- optimizers/sgd.py
+++ optimizers/sgd.py
@@ -1,7 +1,22 @@
-class Optimizer_SGD:
-  def __init__(self, learning_rate=1.0):
-    self.learning_rate = learning_rate
+# Stochastic Gradient Descent optimizer
+class SGD_optimizer:
+    def __init__(self, learning_rate=1.0, decay_rate=0.0):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay_rate = decay_rate
+        self.iterations_count = 0
+
+    # Call once before any parameter updates
+    def pre_update_parameters(self):
+        if self.decay_rate:
+            self.current_learning_rate = self.learning_rate * \
+                (1.0 / (1.0 + self.decay_rate * self.iterations_count))
+
     # Update parameters
-  def params_update(self, layer):
-    layer.weights += -self.learning_rate * layer.dweights
-    layer.biases += -self.learning_rate * layer.dbiases
+    def update_parameters(self, layer):
+        layer.weights += -self.current_learning_rate * layer.dweights
+        layer.biases += -self.current_learning_rate * layer.dbiases
+
+    # Call once after any parameter updates
+    def post_update_parameters(self):
+        self.iterations_count += 1
